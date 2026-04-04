da0dfeabed812ee2991142a16abab306c6783e41
alison <202326047_ar@tesch.edu.mx>
2026-04-04 01:17:39 -0600

    app.js

--- Clima/app.js
+++ Clima/app.js
@@ -27,11 +27,41 @@ function posicionCorrecta(posicion) {
   se ejecuta esta función y usamos una ubicación por defecto.
 */
 function errorUbicacion() {
-  resultado.innerHTML = "⚠️ Permiso de ubicación denegado.<br>Mostrando clima de u>
+  resultado.innerHTML = "⚠️ Permiso de ubicación denegado.<br>Mostrando clima de ubicación por defecto (CDMX).";
 
   // Coordenadas de Ciudad de México
   const lat = 19.4326;
   const lon = -99.1332;
 
   obtenerClima(lat, lon);
+}
+
+function obtenerClima(lat, lon) {
+  // ✅ TU API KEY REAL DE WEATHERAPI
+  const apiKey = "db3296e7d7dd4cd5a2140246263003";
+
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}&lang=es`;
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Respuesta incorrecta de la API");
+      }
+      return response.json();
+    })
+    .then(data => mostrarClima(data))
+    .catch(error => {
+      resultado.innerHTML = "❌ Error al obtener datos del clima.";
+      console.error(error);
+    });
+}
+
+function mostrarClima(data) {
+  resultado.innerHTML = `
+    <h2>${data.location.name}, ${data.location.country}</h2>
+    https:${data.current.condition.icon}
+    <p>🌡️ Temperatura: ${data.current.temp_c} °C</p>
+    <p>☁️ Clima: ${data.current.condition.text}</p>
+    <p>💧 Humedad: ${data.current.humidity}%</p>
+  `;
 }
